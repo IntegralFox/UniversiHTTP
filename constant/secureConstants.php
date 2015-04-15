6461b3568dd227cb8ab60d41948bcd819f46a0fd
IntegralFox <michael.welborn0775@gmail.com>
2015-04-15 16:55:55 -0500

    Add template secure constants file

---
+++ constant/secureConstants.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Define database connection parameters.
+ * KEEP THIS FILE SECURE AND READ ONLY. */
+
+$GLOBALS['DB'] = [
+	'SERVER' => '',
+	'USERNAME' => '',
+	'PASSWORD' => '',
+	'DATABASE' => ''
+];
+
+?>
